864be7acccbdd2301f862e6712c1c869074ddfe5
sergio_valle <sergiovallegarma@gmail.com>
2024-04-07 15:27:20 -0500

    Agregar archivos de proyecto.

---
+++ listas/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] notas = new int[5];
+
+List<string> asistente = new List<string>();
+
+void Ingresar()
+{
+    int cantidad = 0;
+    Console.WriteLine("Ingrese cantidad de notas");
+    cantidad = Convert.ToInt32(Console.ReadLine());
+    //arreglos
+    notas = new int[cantidad];
+
+    for (int i = 0; i < notas.Length; i++)
+    {
+        int numeroNotas = i + 1;
+        Console.WriteLine("Ingrese nota "+ numeroNotas);
+        notas[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Ingresar();
+
+
